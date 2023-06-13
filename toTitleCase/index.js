1947b39fcf6ec5c7d670b6d5021a8a98059682f4
dghousi <dwlatzai.ghousi@gmail.com>
2023-06-13 11:36:34 +0430

    Solve capitalize word and title case sentence challenge

---
+++ toTitleCase/index.js
@@ -0,0 +1,43 @@
+/* toTitleCase
+Write a function that will capitalize every word in a sentence.  
+
+Example Input: "everything, everywhere, all at once"
+Example Output: "Everything, Everywhere, All At Once"
+*/
+
+/* 
+First, write a function that takes in one word and 
+capitalizes the first letter of that word.
+
+Example Input: "scrimba"
+Example Output: "Scrimba"
+
+Hint: Trying using slice() and .toUpperCase()
+*/
+
+function capitalizeWord(word) {
+  return (
+    word.split('').join('').slice(0, 1).toUpperCase() +
+    '' +
+    word.slice(1, word.length)
+  )
+}
+
+/* 
+Now write a function that capitalizes every word in a sentence. 
+How can you reuse the function you just wrote? 
+*/
+
+function toTitleCase(str) {
+  let strArr = str.split(' ')
+
+  let capitalizedStrArr = strArr.map(str => capitalizeWord(str))
+
+  let titleCaseStr = capitalizedStrArr.join(' ')
+
+  return titleCaseStr
+}
+
+// Test your functions
+console.log(capitalizeWord('pumpkin'))
+console.log(toTitleCase('pumpkin pranced purposefully across the pond'))
